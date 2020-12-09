cd162f007f63f6fda85057f4bbf33c5b2e3467a2
sanderheieren <sanheieren@gmail.com>
2020-12-09 16:39:26 +0100

    working, but want to understand from/input relationship with the btn eventlistener

--- hidden-search-widget-trial/script.js
+++ hidden-search-widget-trial/script.js
@@ -1,12 +1,20 @@
 const btn  = document.querySelector('.btn');
 const search = document.querySelector('.search')
+const form = document.querySelector('.form')
 const input = document.querySelector('.input')
-btn.addEventListener('click', () => {
+
+btn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log(input.value);
   search.classList.toggle('active');
   input.focus();
+  input.value ="";
 
 })
 
-input.addEventListener('submit', () => {
-  console.log('sumbitted');
+form.addEventListener('submit', (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log('sumbitted, why am i not comming in here?');
 })
