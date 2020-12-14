4eb3d876aaebfd6b884c31864e201342052e9a79
sanderheieren <sanheieren@gmail.com>
2020-12-14 18:51:31 +0100

    same, async/await

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -5,15 +5,26 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke()
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       "Accept": "application/json"
     }
   }
-  fetch('https://icanhazdadjoke.com', config)
-    .then(res => res.json())
-    .then(data=> {
-      jokeEl.innerHTML = data.joke;
-    })
+  const response = await fetch('https://icanhazdadjoke.com', config);
+  const data = await response.json();
+  jokeEl.innerHTML = data.joke;
 }
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       "Accept": "application/json"
+//     }
+//   }
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then(res => res.json())
+//     .then(data=> {
+//       jokeEl.innerHTML = data.joke;
+//     })
+// }
