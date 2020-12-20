a3f9382f5b4d3123f2dfb8ff809a50ae07c24a4d
sanderheieren <sanheieren@gmail.com>
2020-12-20 14:06:22 +0100

    clean

--- incrementing-counter-trial/script.js
+++ incrementing-counter-trial/script.js
@@ -3,14 +3,12 @@ const counters = document.querySelectorAll('.counter');
 counters.forEach(counter => {
   const target = counter.getAttribute('data-target');
   const increment = target / 200;
-
  
   counter.innerText = '0';
-  console.log(increment);
 
   const updateCounter = () => {
     let innerValue = +counter.innerText;
-  console.log(innerValue);
+
     if(innerValue < target) {
       innerValue += Math.ceil(increment);
       counter.innerText  = innerValue;
