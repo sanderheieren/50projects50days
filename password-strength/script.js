5b94911a8b99b69efff51244c039d33ef2e4d62d
sanderheieren <sanheieren@gmail.com>
2021-01-21 14:39:19 +0100

    working

---
+++ password-strength/script.js
@@ -0,0 +1,11 @@
+const background = document.getElementById('background');
+const password = document.getElementById('password')
+
+
+password.addEventListener('input', (e) => {
+  const input = e.target.value;
+  const length = input.length;
+
+  const blurValue = 20 - length*2
+  background.style.filter = `blur(${blurValue}px)`
+})
